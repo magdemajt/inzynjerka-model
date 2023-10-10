d3aa1218b4186c5d71a612b9301f647a20bfbcd3
hit <tomaszbochnak00@gmail.com>
2023-10-10 22:26:58 +0200

    Added x frame options to localhost:3000

--- lib/inzynjerka_model_web/allow_all_frames.ex
+++ lib/inzynjerka_model_web/allow_all_frames.ex
@@ -1,9 +1,13 @@
-defmodule AllowAllFrames do
+defmodule InzynjerkaModelWeb.AllowAllFrames do
+  @moduledoc """
+  Accepts x-frames from localhost:3000.
+  """
   import Plug.Conn
+
 
   def init(default), do: default
 
-  def call(conn, _default) do
-    put_resp_header(conn, "x-frame-options", "ALLOWALL")
+  def call(conn, _opts) do
+    put_resp_header(conn,"x-frame-options","ALLOW-FROM http://localhost:3000")
   end
 end
