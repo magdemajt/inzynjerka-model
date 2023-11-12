c18fc37b854e64c66cccf42ea786553536c1d5a9
Demolen22 <DemuChannel@gmail.com>
2023-11-12 22:25:14 +0100

    db population

--- lib/inzynjerka_model/chatbot/questions.ex
+++ lib/inzynjerka_model/chatbot/questions.ex
@@ -21,6 +21,11 @@ defmodule InzynjerkaModel.Chatbot.Questions do
     Repo.all(Question)
   end
 
+  def list_questions(limit) do
+    query = from Question, limit: ^limit
+    Repo.all(Question)
+  end
+
   @doc """
   Gets a single question.
 
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,19 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias InzynjerkaModel.Chatbot.Questions
+
+create_questions_for_one_answer = fn %{"questions" => questions, "answer" => answer} ->
+  Enum.map(questions, fn question ->
+    {:ok, new_question} = Questions.create_question(
+      %{language: "Polish",
+        content: question,
+        answer: answer,
+        is_displayed: true}
+    ) end
+  )
+end
+
+with {:ok, body} <- File.read("priv/repo/seed.json"), {:ok, json} <- Jason.decode(body) do
+  Enum.map(json, create_questions_for_one_answer)
+end
